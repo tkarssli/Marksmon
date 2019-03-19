a0c9d81e05bd7dc05ad094654cc22122ac951c76
Tam Kar <tkarssli@gmail.com>
2019-03-18 22:45:59 -0700

    Create target with collision

---
+++ src/target.js
@@ -0,0 +1,38 @@
+import utils from './utils'
+import CollisionBox from './collision_box'
+import * as CONST from './consts'
+
+class Target {
+    constructor(x, y, value) {
+        this.x = x
+        this.y = innerHeight-CONST.FLOOR - 20 + y
+        this.w = 20
+        this.h = 100
+        this.hitBox = new CollisionBox(this.x, this.y, this.w, this.h)
+        this.value = value
+    }
+
+   update(ctx){
+        this.changeY();
+        this.draw(ctx);
+        this.hitBox.update(ctx)
+    }
+
+    changeY(){
+        const newY =  innerHeight-CONST.FLOOR -100- 20;
+        this.y = newY
+        this.hitBox.y = newY
+    }
+
+
+    draw (ctx){
+        
+        ctx.beginPath()
+        ctx.rect(this.x, this.y, this.w, this.h);
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+}
+
+module.exports = Target;
