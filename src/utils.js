811bc8248fc443c014f79971444e2cef93731be2
Tam Kar <tkarssli@gmail.com>
2019-03-18 22:45:37 -0700

    Create utils file

---
+++ src/utils.js
@@ -0,0 +1,25 @@
+function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function randomColor(colors) {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
+
+function distance(x1, y1, x2, y2) {
+    const xDist = x2 - x1
+    const yDist = y2 - y1
+
+    return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+}
+
+function angle(cx, cy, ex, ey) {
+    let dy = ey - cy;
+    let dx = ex - cx;
+    let theta = Math.atan2(dy, dx); // range (-PI, PI]
+    // theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
+    // if (theta < 0) theta = 360 + theta; // range [0, 360)
+    return theta;
+  }
+
+module.exports = { randomIntFromRange, randomColor, distance, angle }
