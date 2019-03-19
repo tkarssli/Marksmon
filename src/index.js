7ccd7081a1c5400a3e2f241dd62f829d407e8d99
Tam Kar <tkarssli@gmail.com>
2019-03-18 22:48:31 -0700

    Add misc files

---
+++ src/index.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const domCanvasEl = document.getElementById("game-canvas");
+    const canvas = domCanvasEl.getContext("2d")
+})
---
+++ src/moving_object.js
@@ -0,0 +1,13 @@
+class MovingObject {
+    constructor(options) {
+        const { pos, vel } = options
+        this.pos = pos;
+        this.vel = vel;
+    }
+
+    draw = () => {
+        
+    }
+}
+
+module.exports = MovingObject;
